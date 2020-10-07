4cc0ccf1622836635fc33012464d44c7cdd5f1e9
Matias Jose Paez Paz <matiasjose.paez@globant.com>
2020-10-07 19:20:19 -0300

    Agregar principio del desafio

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
-console.log('Hola Mundo')
+let texto = prompt('Ingrese un texto, con un máximo de 70 caracteres');
+
+while (texto.length > 70) {
+    texto = prompt('No se pudo procesar. Por favor ingrese un texto que no supere los 70 caracteres.');
+}
+
+console.log('Largo total (contando espacios en blanco): ' + texto.length);
+
+
+contador = 0;
+for(let i = 0; i < texto.length; i++) {
+    if (texto[i] !== " ") {
+        contador++;
+    }
+}
+
+
+console.log('Largo total (sin contar espacio en blanco): ' + contador);
